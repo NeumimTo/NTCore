132b26ad39225787feb6dee4cc219485a11d45a6
NeumimTo <NeumimTo@users.noreply.github.com>
2015-12-19 15:19:11 +0100

    Update SessionFactoryCreatedEvent.java

--- src/main/java/cz/neumimto/core/SessionFactoryCreatedEvent.java
+++ src/main/java/cz/neumimto/core/SessionFactoryCreatedEvent.java
@@ -2,13 +2,14 @@ package cz.neumimto.core;
 
 import org.hibernate.SessionFactory;
 import org.spongepowered.api.event.Event;
+import org.spongepowered.api.event.cause.Cause;
 
 /**
  * Created by NeumimTo on 28.11.2015.
  */
 public final class SessionFactoryCreatedEvent implements Event {
     private final SessionFactory sessionFactory;
-
+    private Cause a = Cause.ofNullable(null);
     public SessionFactoryCreatedEvent(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
@@ -16,4 +17,10 @@ public final class SessionFactoryCreatedEvent implements Event {
     public SessionFactory getSessionFactory() {
         return sessionFactory;
     }
+
+    @Override
+    public Cause getCause() {
+        return a;
+    }
+
 }
